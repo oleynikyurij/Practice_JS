44dc3cfecfe1a51d505e18924a818080ce54d3e9
unknown <D:\\Mail>
2018-06-01 14:16:56 +0300

    add vars

--- jQuery_practice/slider/script.js
+++ jQuery_practice/slider/script.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function () {
+	
+	$('.next').on('click', function () {
+		var currentImage = $('.img.active');
+		var currentImageIndex = $('.img.active').index();
+		var nextImageIndex = currentImageIndex +1;
+		var nextImage = $('.img').eq(nextImageIndex);
+		currentImage.fadeOut(800);
+	});
+});
