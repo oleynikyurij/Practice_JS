ca728e28405d178813c9210eafd243abb0a98ae7
unknown <D:\\Mail>
2018-06-01 14:31:30 +0300

    slider

--- jQuery_practice/slider/script.js
+++ jQuery_practice/slider/script.js
@@ -6,5 +6,27 @@ $(document).ready(function () {
 		var nextImageIndex = currentImageIndex +1;
 		var nextImage = $('.img').eq(nextImageIndex);
 		currentImage.fadeOut(800);
+		currentImage.removeClass('active');
+
+		if ( nextImageIndex == ($('.img:last').index() + 1)) {
+			$('.img').eq(0).fadeIn(800);
+			$('.img').eq(0).addClass('active');
+		} else {
+			nextImage.fadeIn(800);
+			nextImage.addClass('active');
+		}
 	});
+
+  $('.prev').on('click', function () {
+		var currentImage = $('.img.active');
+		var currentImageIndex = $('.img.active').index();
+		var prevImageIndex = currentImageIndex -1;
+		var prevImage = $('.img').eq(prevImageIndex);
+
+		currentImage.fadeOut(800);
+		currentImage.removeClass('active');
+		prevImage.fadeIn(800);
+		prevImage.addClass('active');
+	});
+
 });
