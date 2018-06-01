42ba8137d49c9a752f5eeebcfd32c1f0bac158cf
unknown <D:\\Mail>
2018-06-01 16:23:55 +0300

    to-do add js

--- jQuery_practice/to-do-list/script.js
+++ jQuery_practice/to-do-list/script.js
@@ -4,6 +4,38 @@ $(document).ready(function () {
 	var taskInput = $('#taskInput');
 	var notification = $('#notification');
 
+	var displayNotification = function() {
+		if ( !tasksList.children().length) {
+			notification.fadeIn(400);
+		} else {
+			notification.css("display", "none");
+		}
+	}
+
+	$('#taskAdd').on('click', function () {
+		if ( !taskInput.val()) {
+			return false;
+		}
+
+		tasksList.append("<li>" + taskInput.val() + "<button class='delete'>&#10006</button></li>");
+
+		taskInput.val('');
+
+		displayNotification();
+
+		$('.delete').on('click', function () {
+			var parent = $(this).parent();
+	
+			parent.css("display", "none" );
+	
+			setTimeout(function() {
+				parent.remove();
+				displayNotification();
+			}, 400);
+		});
+
+	});
+
 	
 
 });
